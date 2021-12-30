47c82a92a8a09a6232f121fa0e8a09eba4875fc2
Torhlex <94188402+Torhlex@users.noreply.github.com>
2021-12-30 12:35:28 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,150 @@
+// Question 1 - Declare two variables: firstName and lastName. 1
+// Variables
+let firstName = "Kehinde";
+let lastName = "Odetola";
+
+// Question 2 - Assign the value "Kehinde" to firstName     2
+// let firstName = "Kehinde";
+console.log(firstName);
+
+// Question 3 - Assign the value "Odetola" to   lastName   2
+// let lastName = "Odetola";
+console.log(lastName);
+
+// Question 4 - Copy the value from firstName AND lastName to boss. i.e variable  boss should be equal to Kehinde Odetola  3
+let Boss = "Kehinde Odetola";
+console.log(Boss);
+
+// Question 5 - Show the value of boss using alert .  2
+// let Boss = "Kehinde Odetola";
+alert(Boss);
+console.log(Boss);
+
+// Question 6 - Create a variable with the name of our planet. How would you name such a variable?  2
+let planet = "Earth";
+console.log(planet);
+
+// Question 7 - Create a variable to store the name of a current visitor to a website. How would you name that variable? 2
+let User = "Victor";
+console.log(User);
+
+// Question 8 - create an array arr of 20 items in it 3
+const Countries = [
+  "Nigeria",
+  "Togo",
+  "Congo",
+  "Benin",
+  "Libia",
+  "Ameria",
+  "France",
+  "Algeria",
+  "Argentina",
+  "Botswana",
+  "Brazil",
+  "Chad",
+  "Chile",
+  "China",
+  "Colombia",
+  "Georgia",
+  "Germany",
+  "Madagascar",
+  "Malaysia",
+  "Mali",
+];
+console.log(Countries);
+
+// Question 9 - output the items with positions in a  multiple of 5  in  array  arr      4
+console.log(Countries[4], Countries[9], Countries[14], Countries[19]);
+
+// Question 10 - create an dog Object with 4 properties.  2
+const Identity = {
+  firstName: "Adetola",
+  lastName: "Adewoyin",
+  State: "Osun",
+  Age: 20,
+};
+console.log(Identity);
+
+// Question 11 - create an array of objects of 8  different animals and output 1 of each animal's properties 5
+let animals = [
+  {
+    Name: "Goat",
+    Sex: "Female",
+    Color: "Black",
+    Age: 1,
+  },
+
+  {
+    Name: "Sheep",
+    Sex: "Female",
+    Color: "White",
+    Age: 2,
+  },
+
+  {
+    Name: "Cow",
+    Sex: "Male",
+    Color: "Black",
+    Age: 4,
+  },
+
+  {
+    Name: "Pig",
+    Sex: "Female",
+    Color: "Brown",
+    Age: 2,
+  },
+
+  {
+    Name: "Loin",
+    Sex: "male",
+    Color: "Brown",
+    Age: 3,
+  },
+
+  {
+    Name: "Panter",
+    Sex: "Male",
+    Color: "Black",
+    Age: 2,
+  },
+
+  {
+    Name: "Tiger",
+    Sex: "Male",
+    Color: "Natural",
+    Age: 3,
+  },
+
+  {
+    Name: "Dog",
+    Sex: "Male",
+    Color: "Black",
+    Age: 2,
+  },
+];
+console.log(animals);
+
+// Question 12 -  Create a variable for ptpAmount and write a conditional statement using if and else for different cases of the PTP Amount; Have an output for any possible amount. (be creative here for the possible scenarios of PTP). 5
+let PtpAmount1 = 200000;
+if ((PtpAmount1 = 200000)) {
+  AmountToPay = "true";
+} else if (PtpAmount1 < 200000) {
+  AmountToPay = "False";
+  console.log(PtpAmount);
+}
+
+let PtpAmount2 = 100000;
+if (PtpAmount2 > 100000) {
+  console.log("Ptp Amount is allow");
+} else PtpAmount2 < 100000;
+{
+  console.log("Ptp Amount is not allow");
+}
+
+// Question 13 - Bonus question.. At Bfree Mentorship Programme Which team stands out as the best ? and why?
+// Of course the frontEnd team stands out best. we are one family,
+// communication is made easy and with free access to our instructor makes life easy. Among other things,
+// it is the hyper-activness of our instructors and their passion to inpart others with their knowlegde that makes the front-end team stand out...
+// Their drive and motivate is what drives the team and with the fact that they make front-end as simple as possible...
+// It is really amazing how much one could learn under the right suopervision
